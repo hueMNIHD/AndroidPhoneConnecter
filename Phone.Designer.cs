4a6e1864fd08c7f986223b1a375f84478aa5c1fe
hueMNIHD <outofwallmnihd@gmail.com>
2025-02-03 21:45:08 +0800

    Add files via upload

---
+++ Phone.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace PhoneConnecter
+{
+    partial class Phone
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            textBox1 = new TextBox();
+            button8 = new Button();
+            button9 = new Button();
+            panel1 = new Panel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel2 = new Panel();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            button11 = new Button();
+            panel3 = new Panel();
+            button10 = new Button();
+            checkBox1 = new CheckBox();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            panel2.SuspendLayout();
+            flowLayoutPanel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Gray;
+            button1.Dock = DockStyle.Fill;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(14, 26);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Gray;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(3, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(14, 70);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Gray;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(3, 79);
+            button2.Name = "button2";
+            button2.Size = new Size(14, 70);
+            button2.TabIndex = 3;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Gray;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(3, 3);
+            button4.Name = "button4";
+            button4.Size = new Size(32, 26);
+            button4.TabIndex = 4;
+            button4.Text = "◁";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Gray;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Microsoft YaHei UI", 9F);
+            button5.Location = new Point(41, 3);
+            button5.Name = "button5";
+            button5.Size = new Size(32, 26);
+            button5.TabIndex = 5;
+            button5.Text = "◯";
+            button5.TextAlign = ContentAlignment.BottomCenter;
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Gray;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Location = new Point(79, 3);
+            button6.Name = "button6";
+            button6.Size = new Size(32, 26);
+            button6.TabIndex = 6;
+            button6.Text = "▢";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Gray;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Location = new Point(3, 35);
+            button7.Name = "button7";
+            button7.Size = new Size(54, 26);
+            button7.TabIndex = 7;
+            button7.Text = "Load";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.DimGray;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Location = new Point(3, 8);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(925, 16);
+            textBox1.TabIndex = 8;
+            textBox1.Text = "Phh-Treble with GApps";
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Gray;
+            button8.Dock = DockStyle.Fill;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Location = new Point(1128, 3);
+            button8.Name = "button8";
+            button8.Size = new Size(42, 26);
+            button8.TabIndex = 9;
+            button8.Text = "Exit";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.Gray;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Location = new Point(117, 3);
+            button9.Name = "button9";
+            button9.Size = new Size(54, 26);
+            button9.TabIndex = 11;
+            button9.Text = "Add";
+            button9.UseVisualStyleBackColor = false;
+            button9.Visible = false;
+            button9.Click += button9_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Gray;
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(20, 32);
+            panel1.Margin = new Padding(0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1105, 659);
+            panel1.TabIndex = 10;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 48F));
+            tableLayoutPanel1.Controls.Add(button1, 0, 0);
+            tableLayoutPanel1.Controls.Add(button8, 2, 0);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 1, 0);
+            tableLayoutPanel1.Controls.Add(panel1, 1, 1);
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel2, 0, 1);
+            tableLayoutPanel1.Controls.Add(panel3, 2, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 32F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(1173, 691);
+            tableLayoutPanel1.TabIndex = 12;
+            tableLayoutPanel1.Resize += tableLayoutPanel1_Resize;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 174F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel2.Controls.Add(flowLayoutPanel1, 0, 0);
+            tableLayoutPanel2.Controls.Add(panel2, 1, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(20, 0);
+            tableLayoutPanel2.Margin = new Padding(0);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.Size = new Size(1105, 32);
+            tableLayoutPanel2.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(button4);
+            flowLayoutPanel1.Controls.Add(button5);
+            flowLayoutPanel1.Controls.Add(button6);
+            flowLayoutPanel1.Controls.Add(button9);
+            flowLayoutPanel1.Controls.Add(button7);
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Margin = new Padding(0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(174, 32);
+            flowLayoutPanel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(textBox1);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(174, 0);
+            panel2.Margin = new Padding(0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(931, 32);
+            panel2.TabIndex = 2;
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.Controls.Add(button3);
+            flowLayoutPanel2.Controls.Add(button2);
+            flowLayoutPanel2.Controls.Add(button11);
+            flowLayoutPanel2.Controls.Add(button12);
+            flowLayoutPanel2.Controls.Add(button13);
+            flowLayoutPanel2.Controls.Add(button14);
+            flowLayoutPanel2.Dock = DockStyle.Fill;
+            flowLayoutPanel2.Location = new Point(0, 32);
+            flowLayoutPanel2.Margin = new Padding(0);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(20, 659);
+            flowLayoutPanel2.TabIndex = 11;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.Gray;
+            button11.FlatStyle = FlatStyle.Flat;
+            button11.Location = new Point(3, 155);
+            button11.Name = "button11";
+            button11.Size = new Size(14, 11);
+            button11.TabIndex = 4;
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += button11_Click;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button10);
+            panel3.Controls.Add(checkBox1);
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(1128, 35);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(42, 653);
+            panel3.TabIndex = 12;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.Gray;
+            button10.FlatStyle = FlatStyle.Flat;
+            button10.Location = new Point(2, 27);
+            button10.Name = "button10";
+            button10.Size = new Size(39, 26);
+            button10.TabIndex = 12;
+            button10.Text = "Fill";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(2, 0);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(43, 21);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "RB";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.Gray;
+            button12.FlatStyle = FlatStyle.Flat;
+            button12.Location = new Point(3, 172);
+            button12.Name = "button12";
+            button12.Size = new Size(14, 11);
+            button12.TabIndex = 5;
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.Gray;
+            button13.FlatStyle = FlatStyle.Flat;
+            button13.Location = new Point(3, 189);
+            button13.Name = "button13";
+            button13.Size = new Size(14, 11);
+            button13.TabIndex = 6;
+            button13.UseVisualStyleBackColor = false;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.Gray;
+            button14.FlatStyle = FlatStyle.Flat;
+            button14.Location = new Point(3, 206);
+            button14.Name = "button14";
+            button14.Size = new Size(14, 11);
+            button14.TabIndex = 7;
+            button14.UseVisualStyleBackColor = false;
+            button14.Click += button14_Click;
+            // 
+            // Phone
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DimGray;
+            ClientSize = new Size(1173, 691);
+            Controls.Add(tableLayoutPanel1);
+            DoubleBuffered = true;
+            Name = "Phone";
+            Text = "Phone";
+            FormClosed += Phone_FormClosed;
+            Load += Phone_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            flowLayoutPanel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            flowLayoutPanel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button3;
+        private Button button2;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private TextBox textBox1;
+        private Button button8;
+        private Button button9;
+        private Panel panel1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel panel2;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private CheckBox checkBox1;
+        private Button button10;
+        private Panel panel3;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+    }
+}
---
+++ Phone.cs
@@ -0,0 +1,208 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace PhoneConnecter
+{
+    public partial class Phone : Form
+    {
+        public Phone()
+        {
+            InitializeComponent();
+        }
+
+        string Power = string.Empty, VoiceUp = string.Empty, VoiceDown = string.Empty, Home = string.Empty, Back = string.Empty, History = string.Empty, Scrcpy = string.Empty;
+
+        IntPtr ScrcpyForm = IntPtr.Zero;
+
+        Process MainProcess = new Process();
+
+        Process ScrcpyBackground = new Process();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetParent(IntPtr hChildern, IntPtr hParent);
+
+        [DllImport("user32.dll")]
+        public static extern bool MoveWindow(IntPtr hWnd, int x, int y, int width, int height, bool rePaint);
+
+        [DllImport("user32.dll")]
+        static extern bool GetWindowRect(IntPtr hWnd, ref RECT lpRect);
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        private void Phone_Load(object sender, EventArgs e)
+        {
+            Power = "adb shell input keyevent 26";
+            VoiceUp = "adb shell input keyevent 24";
+            VoiceDown = "adb shell input keyevent 25";
+            Home = "adb shell input keyevent 3";
+            Back = "adb shell input keyevent 4";
+            History = "adb shell input keyevent 187";
+            Scrcpy = "scrcpy";
+            MainProcess.StartInfo.FileName = "cmd.exe";
+            MainProcess.StartInfo.RedirectStandardInput = true;
+            MainProcess.StartInfo.RedirectStandardOutput = true;
+            MainProcess.StartInfo.CreateNoWindow = true;
+            MainProcess.Start();
+            ScrcpyBackground.StartInfo.FileName = "cmd.exe";
+            ScrcpyBackground.StartInfo.RedirectStandardInput = true;
+            ScrcpyBackground.StartInfo.CreateNoWindow = true;
+            ScrcpyBackground.Start();
+            textBox1.Width = panel2.Width - 6;
+            textBox1.Location = new Point(3, textBox1.Location.Y);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainProcess.StandardInput.WriteLine(Power);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MainProcess.StandardInput.WriteLine(VoiceUp);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MainProcess.StandardInput.WriteLine(VoiceDown);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MainProcess.StandardInput.WriteLine(Back);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MainProcess.StandardInput.WriteLine(Home);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MainProcess.StandardInput.WriteLine(History);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MainProcess.StandardInput.WriteLine("taskkill /F /IM adb.exe /T");
+            ScrcpyBackground.StandardInput.WriteLine(Scrcpy);
+            for (int i = 0; i <= 256; i++)
+            {
+                Thread.Sleep(100);
+                if (i == 256)
+                {
+                    MessageBox.Show("超时");
+                    MainProcess.StandardInput.WriteLine("taskkill /F /IM adb.exe /T");
+                    break;
+                }
+                if (FindWindow("SDL_app", textBox1.Text) != IntPtr.Zero)
+                {
+                    ScrcpyForm = FindWindow("SDL_app", textBox1.Text);
+                    SetParent(ScrcpyForm, panel1.Handle);
+                    button7.Visible = false;
+                    button9.Visible = true;
+                    textBox1.Visible = false;
+                    break;
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                MainProcess.StandardInput.WriteLine("adb reboot");
+            }
+            MainProcess.StandardInput.WriteLine("taskkill /F /IM adb.exe /T");
+            Application.Exit();
+            Environment.Exit(0);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Process newProcess = new Process();
+            newProcess.StartInfo.FileName = "cmd.exe";
+            newProcess.StartInfo.RedirectStandardInput = true;
+            newProcess.StartInfo.CreateNoWindow = true;
+            newProcess.Start();
+            newProcess.StandardInput.WriteLine(Scrcpy);
+            for (int i = 0; i <= 256; i++)
+            {
+                Thread.Sleep(100);
+                if (i == 256)
+                {
+                    MessageBox.Show("超时");
+                    MainProcess.StandardInput.WriteLine("taskkill /F /IM adb.exe /T");
+                    break;
+                }
+                if (FindWindow("SDL_app", textBox1.Text) != IntPtr.Zero)
+                {
+                    ScrcpyForm = FindWindow("SDL_app", textBox1.Text);
+                    SetParent(ScrcpyForm, panel1.Handle);
+                    break;
+                }
+            }
+        }
+
+        private void tableLayoutPanel1_Resize(object sender, EventArgs e)
+        {
+            textBox1.Width = panel2.Width - 6;
+            textBox1.Location = new Point(3, textBox1.Location.Y);
+        }
+
+        private void Phone_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MainProcess.StandardInput.WriteLine("taskkill /F /IM adb.exe /T");
+            Application.Exit();
+            Environment.Exit(0);
+        }
+
+        bool HasMaxed = false;
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (HasMaxed)
+            {
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                this.WindowState = FormWindowState.Normal;
+                HasMaxed = false;
+            }
+            else
+            {
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.WindowState = FormWindowState.Maximized;
+                HasMaxed = true;
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            MainProcess.StandardInput.WriteLine("adb shell settings put system user_rotation 0");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            MainProcess.StandardInput.WriteLine("adb shell settings put system user_rotation 1");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            MainProcess.StandardInput.WriteLine("adb shell settings put system user_rotation 2");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            MainProcess.StandardInput.WriteLine("adb shell settings put system user_rotation 3");
+        }
+    }
+}
